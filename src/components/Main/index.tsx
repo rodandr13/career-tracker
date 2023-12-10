d4a6f37bb031a77b11f4464ecbbfe46af5607f0d
Andrey Roditelev <rodandr13@gmail.com>
2023-12-10 13:57:26 +0300

    feat: add route

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import styles from './Main.module.scss';
+import Strategy from '../../pages/Strategy';
+import SkillsAssessment from '../../pages/SkillsAssessment';
 import HelpDecide from '../../pages/HelpDecide';
 
 function Main() {
   return (
     <main className={styles.main}>
-      <HelpDecide />
+      <Routes>
+        <Route path="/" element={<Strategy />} />
+        <Route path="/" element={<SkillsAssessment />} />
+        <Route path="/" element={<Strategy />} />
+        <Route path="/" element={<HelpDecide />} />
+      </Routes>
     </main>
   );
 }
