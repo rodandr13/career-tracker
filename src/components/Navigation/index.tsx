d3b59136c7cb57b5ef30deab959eabef3244b9e8
Andrey Roditelev <rodandr13@gmail.com>
2023-12-11 01:41:13 +0300

    feat: add main menu

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -6,32 +6,55 @@ function Navigation() {
     <nav className={styles.navigation}>
       <ul className={styles.menuList}>
         <li className={styles.menuList__item}>
-          <NavLink className={styles.menuList__link} to="/">
+          <NavLink
+            className={`${styles.menuList__link} ${styles.menuList__link_type_vacancies}`}
+            to="/"
+          >
             Вакансии
           </NavLink>
         </li>
         <li className={styles.menuList__item}>
-          <NavLink className={styles.menuList__link} to="/">
+          <NavLink
+            className={`${styles.menuList__link} ${styles.menuList__link_type_workshop}`}
+            to="/"
+          >
             Мастерская
           </NavLink>
         </li>
         <li className={styles.menuList__item}>
-          <NavLink className={styles.menuList__link} to="/">
+          <NavLink
+            className={`${styles.menuList__link} ${styles.menuList__link_type_diary}`}
+            to="/"
+          >
             Дневник
           </NavLink>
         </li>
         <li className={styles.menuList__item}>
-          <NavLink className={styles.menuList__link} to="/">
+          <NavLink
+            className={({ isActive }) =>
+              [
+                `${styles.menuList__link} ${styles.menuList__link_type_progressTracker}`,
+                isActive ? `${styles.menuList__link_active}` : '',
+              ].join(' ')
+            }
+            to="/progress-tracker"
+          >
             Трекер развития
           </NavLink>
         </li>
         <li className={styles.menuList__item}>
-          <NavLink className={styles.menuList__link} to="/">
+          <NavLink
+            className={`${styles.menuList__link} ${styles.menuList__link_type_contacts}`}
+            to="/"
+          >
             Контакты
           </NavLink>
         </li>
         <li className={styles.menuList__item}>
-          <NavLink className={styles.menuList__link} to="/">
+          <NavLink
+            className={`${styles.menuList__link} ${styles.menuList__link_type_feed}`}
+            to="/"
+          >
             Лента
           </NavLink>
         </li>
